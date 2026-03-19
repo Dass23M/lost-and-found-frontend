92a7d2062e88d4e9f792bbb1d0d4ead8dd57a16e
Dass23M <157539720+Dass23M@users.noreply.github.com>
2026-03-19 12:38:20 +0530

    update Cros Origin

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,6 +3,7 @@ import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
@@ -12,5 +13,15 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove("token");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
